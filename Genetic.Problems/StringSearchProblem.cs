1cd3beb611c4adf5519415845be9e2e6e9746652
pdrozdowski <pdrozdo82@gmail.com>
2015-07-28 14:03:18 +0200

    String search problem defined and added stub implementation of it proposing usage of the library

--- Genetic.Problems/StringSearchProblem.cs
+++ Genetic.Problems/StringSearchProblem.cs
@@ -6,11 +6,12 @@ namespace Genetic.Problems
     /*
     * I would like to have an evolution algorithm
     * using sequence of characters as a representation
-    * where each character can be in range a-Z.
+    * where each character can be in range a-Z and !.
     * Length of genome will be fixed to length of passphrase.
-    * Fitness will be calculated as distance of genome to 'GenticAlgorithmsWorks'.
+    * Fitness will be calculated as distance of genome to 'GenticAlgorithmsWorks!'.
+    * 
     * Mutation rate should be 1% and single-cut crossing should be applied.
-    * For selection I would like to use 'keep fittest' strategy.
+    * For selection I would like to use 'keep most fit' strategy.
     * I would like population to be not bigger than 100 chromosomes per step.
     * 
     * It is expected to find a perfect match.
@@ -22,7 +23,38 @@ namespace Genetic.Problems
         [TestMethod]
         public void SolveStringSearchProblem()
         {
-            
+            string passphrase = "GenticAlgorithmsWorks!";
+            var random = new RandomNumberGenerators.Default();
+
+            var genePool = GenePool<char>();
+            genePool.AddRange("abcdefghijklmnopqrstuvwxyz".ToCharArray());
+            genePool.AddRange("ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray());
+            genePool.Add('!');
+                        
+            var chromosomeFactory = Chromosomes.SequentialChromosomeFactory<char>(random, genePool, passphrase.Length); // chromosome as fixed lenght sequence with array of allowed characters
+            var fitnessCalc = new FitnessCalculators.StringComparer(passphrase);
+            var mutationStrategy = MutationStrategies.SingleGeneMutation(random, 0.01); // mutation rate fixed to 1%
+            var crossoverStrategy = CrossoverStrategies.SingleCut(random);
+            var selectionStrategy = SelectionStrategies.KeepFittest();
+            var populationStrategy = PopulationStrategies.SinglePopulation(100); // size fixed to 100
+
+            var evolution = new Evolution(random, 
+                chromosomeFactory,
+                populationStrategy,
+                mutationStrategy,
+                crossoverStrategy,
+                selectionStrategy);
+
+            do
+            {
+                evolution.Step();
+                if (evolution.TopFitness == 1.0)
+                {
+                    return;
+                }
+            }
+            while (evolution.Generation < 1000);
+            Assert.Fail();
         }
     }
 }
